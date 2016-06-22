5a5a3a668bf818d87090728855581fcbb51a849a
Sam Sortais <sam.sortais@xilinx.com>
2016-06-21 18:08:38 -0700

    generic: handle baremetal dependencies
    cmake check_include_files() tries to build an executable by default,
    this works for linux when it is both host and target.
    Building libmetal for baremetal with no tests/examples shall not require libxil.a
    
    Signed-off-by: Sam Sortais <sam.sortais@xilinx.com>

--- cmake/depends.cmake
+++ cmake/depends.cmake
@@ -1,9 +1,9 @@
-check_include_files (stdatomic.h HAVE_STDATOMIC_H)
-check_include_files (linux/futex.h HAVE_FUTEX_H)
-
 find_package (Doxygen)
 
 if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
+
+  check_include_files (stdatomic.h HAVE_STDATOMIC_H)
+  check_include_files (linux/futex.h HAVE_FUTEX_H)
 
   find_package (HugeTLBFS REQUIRED)
   collect (PROJECT_INC_DIRS "${HUGETLBFS_INCLUDE_DIR}")
@@ -18,6 +18,15 @@ if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
 
   find_package(LibRt REQUIRED)
   collect (PROJECT_LIB_DEPS "${LIBRT_LIBRARIES}")
+
+else ("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
+
+  # TODO: fix for find_path() to detect stdatomic.h 
+  # find_path (HAVE_STDATOMIC_H stdatomic.h)
+  set (_saved_cmake_required_flags ${CMAKE_REQUIRED_FLAGS})
+  set (CMAKE_REQUIRED_FLAGS "-c" CACHE STRING "")
+  check_include_files (stdatomic.h HAVE_STDATOMIC_H)
+  set (CMAKE_REQUIRED_FLAGS ${_saved_cmake_required_flags})
 
 endif ("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
 
