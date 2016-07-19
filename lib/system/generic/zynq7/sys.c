4f7004ce3f9fea6dfe3dfc04cd79d4dc49058046
Wendy Liang <jliang@xilinx.com>
2016-07-18 17:32:44 -0700

    Zynq7: Add default generic system poll
    Introduce default generic system poll function to allow to
    wait until some event happends.
    This function can be overwritten by user outside libmetal.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- lib/system/generic/zynq7/sys.c
+++ lib/system/generic/zynq7/sys.c
@@ -52,3 +52,11 @@ void metal_machine_cache_invalidate(void *addr, unsigned int len)
 		Xil_DCacheInvalidateRange((intptr_t)addr, len);
 }
 
+/**
+ * @brief poll function until some event happens
+ */
+void __attribute__((weak)) metal_generic_default_poll(void)
+{
+	asm volatile("wfi");
+}
+
