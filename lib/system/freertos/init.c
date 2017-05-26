baaead3bb35921f9d5cc76c4cdbd619c2ef4152b
Wendy Liang <jliang@xilinx.com>
2017-05-26 11:06:39 -0700

    FreeRTOS/generic: register generic bus in metal_init
    Register generic bus in the metal_init() for generic and
    FreeRTOS system.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>

--- lib/system/freertos/init.c
+++ lib/system/freertos/init.c
@@ -44,10 +44,12 @@ struct metal_state _metal;
 int metal_sys_init(const struct metal_init_params *params)
 {
 	metal_unused(params);
+	metal_bus_register(&metal_generic_bus);
 	return metal_irq_init();
 }
 
 void metal_sys_finish(void)
 {
 	metal_irq_deinit();
+	metal_bus_unregister(&metal_generic_bus);
 }
