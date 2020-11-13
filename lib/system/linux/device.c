6e81fb67c76b695dc9043d17a3bf4e96c40b0e86
Leo Sartre <sartre.l@ecagroup.com>
2020-11-13 06:43:51 +0100

    lib/system/linux: fix memory leak in metal_uio_read_map_attr()
    The sysfs attribute must be closed before metal_uio_read_map_attr()
    function returns otherwise a resource is leaked.
    
    Signed-off-by: Leo Sartre <sartre.l@ecagroup.com>

--- lib/system/linux/device.c
+++ lib/system/linux/device.c
@@ -87,10 +87,14 @@ static int metal_uio_read_map_attr(struct linux_device *ldev,
 	if (result >= (int)sizeof(path))
 		return -EOVERFLOW;
 	attr = sysfs_open_attribute(path);
-	if (!attr || sysfs_read_attribute(attr) != 0)
+	if (!attr || sysfs_read_attribute(attr) != 0) {
+		sysfs_close_attribute(attr);
 		return -errno;
+	}
 
 	*value = strtoul(attr->value, NULL, 0);
+
+	sysfs_close_attribute(attr);
 	return 0;
 }
 
