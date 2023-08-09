6d571fd068e1998a39a39c1d18d5ff10195274e9
Ben Levinsky <ben.levinsky@amd.com>
2023-08-09 09:11:01 -0700

    lib: system: freertos: Add metal/errno.h to match standalone
    As lib/system/generic/sys.h has metal/errno.h add this to the freertos
    equivalent.
    
    This fixes some compilation issues when linking against certain FreeRTOS port
    BSPs.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@amd.com>

--- lib/system/freertos/sys.h
+++ lib/system/freertos/sys.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016, Xilinx Inc. and Contributors. All rights reserved.
+ * Copyright (c) 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -15,6 +16,8 @@
 
 #ifndef __METAL_FREERTOS_SYS__H__
 #define __METAL_FREERTOS_SYS__H__
+
+#include <metal/errno.h>
 
 #include "./@PROJECT_MACHINE@/sys.h"
 
