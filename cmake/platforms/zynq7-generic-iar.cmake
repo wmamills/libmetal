b691eee88fe0e7fc9c6d5996dd5559960c04459a
Rajiv Mohan <rajiv.mohan@amd.com>
2024-08-02 02:31:06 -0700

    zynq: Remove support for Zynq-7000 SoC
    Remove support for Zynq-7000 SoC, changes are
    1. Remove Zynq7 tool chain support
    2. Makefile changes to remove zynq7
    
    Reasons to remove:
    1. Removing redundant or unmaintained code
    2. Reduce technical debt carried by OpenAMP team
    3. very few customer using openamp on Zynq-7000 SoC
    
    For using Zynq-7000 SoC support last working and tested
    version is (v2023.04)
    https://github.com/OpenAMP/libmetal/tree/v2023.04
    
    Signed-off-by: Rajiv Mohan <rajiv.mohan@amd.com>
    Signed-off-by: Tanmay Shah <tanmay.shah@amd.com>

--- cmake/platforms/zynq7-generic-iar.cmake
+++
@@ -1,5 +0,0 @@
-set (CMAKE_SYSTEM_PROCESSOR "arm" CACHE STRING "")
-set (PROJECT_VENDOR         "xlnx"             CACHE STRING "")
-set (CROSS_SUFFIX           "arm" CACHE STRING "")
-include (cross-generic-iar)
-
