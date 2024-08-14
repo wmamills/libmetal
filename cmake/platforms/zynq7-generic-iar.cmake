3e8be081fc38fb6ca7c073aa730320062e5fa6dd
Ben Levinsky <ben.levinsky@amd.com>
2024-08-14 07:51:51 -0700

    cmake: platforms: xlnx: Add PROJECT_VENDOR
    Add PROJECT_VENDOR to Xilinx-AMD .cmake platform files.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@amd.com>

--- cmake/platforms/zynq7-generic-iar.cmake
+++ cmake/platforms/zynq7-generic-iar.cmake
@@ -1,4 +1,5 @@
 set (CMAKE_SYSTEM_PROCESSOR "arm" CACHE STRING "")
+set (PROJECT_VENDOR         "xlnx"             CACHE STRING "")
 set (CROSS_SUFFIX           "arm" CACHE STRING "")
 include (cross-generic-iar)
 
